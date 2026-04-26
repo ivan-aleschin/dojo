9df3f8e06055a3a642412400c85119a3db473b8d
ivan-aleschin <aleshin.ik@phystech.edu>
2026-04-26 21:09:28 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Dojo Environment for Code Kata (C++, C#, Python, Qt6)";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # --- C/C++ ---
+              gcc
+              gnumake
+              cmake
+              clang-tools
+              pkg-config
+              # --- Qt6 для GUI ---
+              qt6.qtbase
+              qt6.qtwayland
+              qt6.qtimageformats
+              qt6.wrapQtAppsHook
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              gdb
+              valgrind
+            ] ++ [
+              # --- C# ---
+              dotnet-sdk
+              # --- Python ---
+              python3
+              uv
+            ];
+            shellHook = ''
+              echo "🛠️ Dojo Environment Loaded (C/C++, C#, Python, Qt6) [${system}]"
+            '';
+          };
+        }
+      );
+    };
+}
