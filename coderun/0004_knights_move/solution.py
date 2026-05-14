a18358d02c5745aac75ec644413e6285e8e68d4e
ivan-aleschin <aleshin.ik@phystech.edu>
2026-05-15 01:34:31 +0300

    chore: test migration

---
+++ coderun/0004_knights_move/solution.py
@@ -0,0 +1,15 @@
+from stat import SF_DATALESS
+
+
+def main():
+    n, m = map(int, input().split())
+    grid = []
+    for i in rabge(n):
+        grid.append(list(map(int, input().split())))
+    count = 0
+
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
